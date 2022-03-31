de744579cb74b2a54bc267fc6d7fe47ca6ef280b
viraksok <sokvirak1606@gmail.com>
2022-03-31 09:48:22 +0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,7 @@ export default function Home() {
       if (todo !== '') {
         if (!isEdit) {
           if (!todolist.some((el) => el.todo == todo)) {
+            setTodolist(fetchData());
             todolist.push({
               id: todolist.length + 1,
               todo: todo,
